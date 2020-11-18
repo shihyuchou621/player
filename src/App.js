c1a416fb0a95347175a67281dc004f75de394bdb
shihyuchou <kellyu621@gmail.com>
2020-11-18 21:40:15 +0800

    App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { createStore } from "redux";
-
+import { Provider } from "react-redux";
 import { Status, Mode } from "./consts";
 import {
   toggleStar,
@@ -12,37 +12,46 @@ import {
   modeChange,
 } from './actions';
 import reducers from "./reducers";
+import Song from './component/Song';
+import SongBar from './component/SongBar';
+import Controler from './component/Controler';
+import SongMode from './component/SongMode';
 
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-store.dispatch(play(1));
+// store.dispatch(play(1));
 
-store.dispatch(pause());
+// store.dispatch(pause());
 
-store.dispatch(resume());
+// store.dispatch(resume());
 
-store.dispatch(ticking());
+// store.dispatch(ticking());
 
-store.dispatch(none());
+// store.dispatch(none());
 
+// store.dispatch(toggleStar(2));
 
-store.dispatch(toggleStar(2));
-
-store.dispatch(
-  modeChange({mode: Mode.ALL})
-);
-
-
+// store.dispatch(
+//   modeChange({mode: Mode.ALL})
+// );
 
 // console.log(store.getState());
 
 export default function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="songArea">
       player
-    </div>
+        <Song num={0} />
+        <Song num={1} />
+        <Song num={2} />
+      </div>
+      <SongBar />
+      <Controler />
+      <SongMode />
+    </Provider>
   );
 }
